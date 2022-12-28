9c19b96193c2bf2357534d7a4678c31bc39d9e77
Pixfri <jean.letessier@protonmail.com>
2022-12-28 15:23:35 +0100

    Base Code to create a Window

--- VulkanTutorial/src/main.cpp
+++ VulkanTutorial/src/main.cpp
@@ -1,7 +1,61 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+
+const uint32_t WIDTH {800};
+const uint32_t HEIGHT {600};
+
+class HelloTriangleApplication {
+public:
+    void run() {
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
+    }
+
+private:
+        GLFWwindow* m_window;
+
+    void initWindow() {
+            glfwInit();
+
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+            m_window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+            while (!glfwWindowShouldClose(m_window)) {
+                    glfwPollEvents();
+            }
+    }
+
+    void cleanup() {
+            glfwDestroyWindow(m_window);
+
+            glfwTerminate();
+    }
+};
 
 int main() {
-        std::cout << "Hello World!" << std::endl;
+        HelloTriangleApplication app {};
 
-        return 0;
+        try {
+                app.run();
+        } catch (const std::exception& e){
+                std::cerr << e.what() << std::endl;
+                return EXIT_FAILURE;
+        }
+
+        return EXIT_SUCCESS;
 }
